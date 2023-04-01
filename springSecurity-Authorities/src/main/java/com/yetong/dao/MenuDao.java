6dfcd03fb0582a7c7c07e8e60bdbadb1e5ceec61
Yetong <Yetong@.com>
2023-04-01 17:14:20 +0800

    security

---
+++ springSecurity-Authorities/src/main/java/com/yetong/dao/MenuDao.java
@@ -0,0 +1,11 @@
+package com.yetong.dao;
+
+import com.yetong.entity.Menu;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface MenuDao {
+    List<Menu> getAllMenu();
+}
